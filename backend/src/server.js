122315e956e581286326d09b152968b69d0f9e3b
saadatwani <saadatmuzaffar4@gmail.com>
2026-05-04 13:12:34 +0530

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,8 @@
 import express, { Router } from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import path from "path";
+import { fileURLToPath } from 'url';
 
 import notesRoutes from './routes/notesRoutes.js';
 import connectDB from './config/db.js';
@@ -12,10 +14,16 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-//middleware:
-app.use(cors({
-    origin: "http://localhost:5173",
-}));
+// Properly construct __dirname inside ES Modules
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+//middleware(only for use in development):
+if (process.env.NODE_ENV !== "production") {
+    app.use(cors({
+        origin: "http://localhost:5173",
+    }));
+}
 
 app.use(express.json());
 app.use((req, res, next) => {
@@ -27,6 +35,16 @@ app.use(rateLimiter);
 
 //response:
 app.use("/api/notes", notesRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    // Calculate the path properly based on the location of this file
+    const frontendPath = path.join(__dirname, "../../frontend/dist");
+    app.use(express.static(frontendPath));
+    
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(frontendPath, "index.html"));
+    });
+}
 
 //connect to DB and start server, in that order
 connectDB().then(() => {
